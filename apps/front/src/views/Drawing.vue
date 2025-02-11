55c8a5b1fd252585ebdbba28c6d1117961102603
Melvin <melvin.courant@lasuiteandco.com>
2025-02-11 17:57:06 +0100

    :lipstick: add background pattern and canvas in drawing page

--- apps/front/src/views/Drawing.vue
+++ apps/front/src/views/Drawing.vue
@@ -1,7 +1,14 @@
 <script setup>
+import '../assets/css/views/_drawing.scss';
+import {useI18n} from "vue-i18n";
 
+const { t } = useI18n();
 </script>
 
 <template>
-  <h1>Drawing</h1>
+  <main class="drawing">
+    <h1 class="hidden-title">{{ t("drawing") }}</h1>
+
+    <canvas></canvas>
+  </main>
 </template>
