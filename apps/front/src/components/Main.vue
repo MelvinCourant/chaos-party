06fafbfbe801b6d2a620dfa975320c1eeb56f1e5
Melvin <melvin.courant@lasuiteandco.com>
2025-03-06 17:37:44 +0100

    :children_crossing: mute music intro in other pages then home, lobby and creating teams

--- apps/front/src/components/Main.vue
+++ apps/front/src/components/Main.vue
@@ -2,9 +2,11 @@
 import Sound from './utils/Sound.vue';
 import { useSettingsStore } from '../stores/settings.js';
 import { reactive, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
 const settingsStore = useSettingsStore();
 const { settings } = settingsStore;
+const route = useRoute();
 const musicAttributes = reactive({
   muted: true,
   loop: true,
@@ -13,12 +15,27 @@ const musicAttributes = reactive({
   id: 'music-intro',
 });
 
+const musicIntroRoutes = ['/', '/lobby', '/creating-teams'];
+
 watch(settings, (value) => {
   musicAttributes.volume = value.musicVolume;
-  musicAttributes.muted = value.musicVolume === 0;
+
+  if (musicIntroRoutes.includes(route.path)) {
+    musicAttributes.muted = value.musicVolume === 0;
+  }
 });
 
-musicAttributes.muted = settings.musicVolume === 0;
+if (musicIntroRoutes.includes(route.path)) {
+  musicAttributes.muted = settings.musicVolume === 0;
+}
+
+watch(route, (value) => {
+  if (musicIntroRoutes.includes(value.path) && settings.musicVolume > 0) {
+    musicAttributes.muted = false;
+  } else {
+    musicAttributes.muted = true;
+  }
+});
 </script>
 
 <template>
